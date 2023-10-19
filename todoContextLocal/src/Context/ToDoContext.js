c8a4ec214c6b2b8ba29bfc9d00c02ae767154ea7
Ritika <128600645+ri-spec@users.noreply.github.com>
2023-10-19 16:57:08 +0000

    Context and local storage

---
+++ todoContextLocal/src/Context/ToDoContext.js
@@ -0,0 +1,23 @@
+import {createContext, useContext} from 'react'
+
+export const ToDoContext = createContext({
+    todos : [
+        {
+            id: 1,
+            title: '',
+            completed: false
+        }
+    ],
+    addToDo: (todo) => {},
+    updateToDo: (id, todo) => {},
+    deleteToDo: (id) => {},
+    toggleComplete: (id) => {}
+
+})
+
+
+export const useToDo = () => {
+    return useContext(ToDoContext)
+}
+
+export const ToDoProvider = ToDoContext.Provider
